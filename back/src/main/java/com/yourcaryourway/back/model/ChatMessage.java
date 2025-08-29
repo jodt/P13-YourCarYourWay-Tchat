a00c860036eaeb3fc52ccfac8e9090da5aeb892a
joel Dumortier <joel.dumortier@gmail.com>
2025-08-29 09:42:51 +0200

    create models

---
+++ back/src/main/java/com/yourcaryourway/back/model/ChatMessage.java
@@ -0,0 +1,28 @@
+package com.yourcaryourway.back.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    private String sender;
+    private LocalDateTime timestamp;
+    private Long roomId;
+
+}
---
+++ back/src/main/java/com/yourcaryourway/back/model/User.java
@@ -0,0 +1,22 @@
+package com.yourcaryourway.back.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "users", uniqueConstraints =  {@UniqueConstraint(columnNames = "username")})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+}
